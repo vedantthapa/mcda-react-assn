3ca4987a803653d4a908453f7d9f38f56da85837
vedantthapa <thapavedant@gmail.com>
2021-12-12 11:44:53 -0400

    Adds more information in About.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,11 @@ export default function About() {
         <h1>Hi👋, I'm Vedant</h1>
         <p>
           I'm currently pursuing my Master's in Computing and Data Analytics @
-          SMU
+          SMU. Previously, I've worked as Data Analyst Intern at multiple
+          start-ups wherein my primary role was to surf datasets quickly,
+          encountering and surfacing potential insights. Also, present this
+          information to the stakeholders using effective plots to allow the
+          mind to extract details faster.
         </p>
         <p>
           If you'd like to get in touch with me, feel free to send me an email
